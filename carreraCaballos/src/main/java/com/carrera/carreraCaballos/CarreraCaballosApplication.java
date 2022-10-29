4bc308e95642bb75d03464a86b3ba483b4f39530
Patricia Sanz Escurin <patricia.sanz.escurin@gmail.com>
2022-10-29 19:24:30 +0200

    Proyecto con Spring Boot creado

---
+++ carreraCaballos/src/main/java/com/carrera/carreraCaballos/CarreraCaballosApplication.java
@@ -0,0 +1,13 @@
+package com.carrera.carreraCaballos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarreraCaballosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarreraCaballosApplication.class, args);
+	}
+
+}
---
+++ carreraCaballos/src/test/java/com/carrera/carreraCaballos/CarreraCaballosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrera.carreraCaballos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarreraCaballosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
